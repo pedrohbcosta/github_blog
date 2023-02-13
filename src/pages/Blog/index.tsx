dfff6e697aa1491e63309ee54edc92c46190ca3c
Pedro Batista <pedro@yellowberry.com.br>
2023-02-13 09:37:10 -0300

    first commit

---
+++ src/pages/Blog/index.tsx
@@ -0,0 +1,7 @@
+export function Blog() {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
---
+++ src/styles/theme/default.tsx
@@ -0,0 +1,27 @@
+export const defaultTheme = {
+  colors: {
+    'base-input': '#040F1A',
+    'base-background': '#071422',
+    'base-profile': '#0b1b2b',
+    'base-post': '#112131',
+    'base-border': '#1c2f41',
+    'base-label': '#3a536b',
+
+    'base-span': '#7b96b2',
+    'base-text': '#afc2d4',
+    'base-subtitle': '#c4d4e3',
+    'base-title': '#e7edf4',
+
+    'brand-blue': '#3294f8',
+  },
+  textSize: {
+    'title-large': '1.5rem',
+    'title-medium': '1.25rem',
+    'title-small': '1.125rem',
+
+    'text-medium': '1rem',
+    'text-small': '0.875rem',
+
+    'component-link': '0.75rem',
+  },
+} as const
