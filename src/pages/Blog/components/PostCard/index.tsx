52eb0a78a671db66daf648d52d01992bda615f39
Pedro Batista <pedro@yellowberry.com.br>
2023-02-13 14:39:04 -0300

    finished post card component. Finished blog home page.

---
+++ src/pages/Blog/components/PostCard/index.tsx
@@ -0,0 +1,22 @@
+import { PostCardContainer } from './styles'
+
+export function PostCard() {
+  return (
+    <PostCardContainer>
+      <div>
+        <strong>JavaScript data types and data structures</strong>
+        <span>Há 1 dia</span>
+      </div>
+      <p>
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in Programming languages all have
+        built-in data structures, but these often differ from one language to
+        another. This article attempts to list the built-in data structures
+        available inProgramming languages all have built-in data structures, but
+        these often differ from one language to another. This article attempts
+        to list the built-in data structures available in
+      </p>
+    </PostCardContainer>
+  )
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,21 @@
+import { PostCard } from './components/PostCard'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput'
+import { PostListContainer } from './styles'
 
 export function Blog() {
   return (
     <div>
       <Profile />
       <SearchInput />
+      <PostListContainer>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </PostListContainer>
     </div>
   )
 }
